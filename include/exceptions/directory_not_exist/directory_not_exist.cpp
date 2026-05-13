097739669c114dc103d85cf3c00519a1b17f883b
nadutudor <nadu.tudor@gmail.com>
2026-05-13 14:24:29 +0300

    fixed errors/warnings

--- include/exceptions/directory_not_exist/directory_not_exist.cpp
+++ include/exceptions/directory_not_exist/directory_not_exist.cpp
@@ -1,6 +1,6 @@
 #include "directory_not_exist.hpp"
 
-explicit DirectoryNotExist::DirectoryNotExist(const std::string parent_directory) : message(parent_directory+ " does not exist \n") {}
+DirectoryNotExist::DirectoryNotExist(const std::string parent_directory) : message(parent_directory+ " does not exist \n") {}
 
 const char* DirectoryNotExist::what() const noexcept {
     return message.c_str();
--- include/exceptions/not_type_directory/not_type_directory.cpp
+++ include/exceptions/not_type_directory/not_type_directory.cpp
@@ -1,6 +1,6 @@
 #include "not_type_directory.hpp"
 
-explicit NotTypeDirectory::NotTypeDirectory(const std::string parent_directory) : message(parent_directory+ " is not a directory \n") {}
+NotTypeDirectory::NotTypeDirectory(const std::string parent_directory) : message(parent_directory+ " is not a directory \n") {}
 
 const char* NotTypeDirectory::what() const noexcept {
     return message.c_str();
